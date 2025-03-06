6d64c2a0ff1029e80ebc5686ae16f920315991c4
ivanhoe <corsair954@mail.ru>
2025-03-06 21:51:41 +0300

    6. Перевернуть связанный список [#161 #535091]

--- src/main/java/ru/job4j/collection/RevertLinked.java
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -20,20 +20,22 @@ public class RevertLinked<T> implements Iterable<T> {
     }
 
     public boolean revert() {
+        boolean result = true;
         if (head == null || head.next == null) {
-            return false;
+            result = false;
+        } else {
+            Node<T> prev = null;
+            Node<T> curr = head;
+            Node<T> next;
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
         }
-        Node<T> prev = null;
-        Node<T> curr = head;
-        Node<T> next;
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        head = prev;
-        return true;
+        return result;
     }
 
     @Override
