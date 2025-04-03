9bf1f54b8afc0c27f2a76dfc7fc8a1ea96b2570b
ivanhoe <corsair954@mail.ru>
2025-04-03 18:39:42 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,8 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -12,11 +14,16 @@ public class EchoServer {
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String s = input.readLine();
+                    Pattern p = Pattern.compile("=\\S+");
+                    Matcher matcher = p.matcher(s);
+                    matcher.find();
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     System.out.println(s);
-                    if (s.contains("msg=Bye")) {
-                        server.close();
+                    switch (matcher.group().substring(1)) {
+                        case "Hello" -> output.write("Hello".getBytes());
+                        case "Exit" -> server.close();
+                        default -> output.write("What".getBytes());
                     }
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
