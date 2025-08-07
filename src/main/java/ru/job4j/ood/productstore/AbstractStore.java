e538de638301d12b1c04a09cb62e96cd9b90369b
ivanhoe <corsair954@mail.ru>
2025-08-07 16:54:40 +0300

    2. Парковка машин [#853 #549965]

--- src/main/java/ru/job4j/ood/productstore/AbstractStore.java
+++ src/main/java/ru/job4j/ood/productstore/AbstractStore.java
@@ -1,6 +1,8 @@
 package ru.job4j.ood.productstore;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public abstract class AbstractStore implements Store {
@@ -9,7 +11,7 @@ public abstract class AbstractStore implements Store {
     protected double calculateRemainder(Food food) {
         long createDate = food.getCreateDate().getTimeInMillis();
         long expireDate = food.getExpireDate().getTimeInMillis();
-        long now = System.currentTimeMillis();
+        long now = new GregorianCalendar(2025, Calendar.AUGUST, 6, 15, 35).getTimeInMillis();
         return (double) (now - createDate) / (expireDate - createDate);
     }
 
