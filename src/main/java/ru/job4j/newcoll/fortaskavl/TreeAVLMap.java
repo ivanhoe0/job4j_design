c0e407b9485eb0db8e0f0e6e2e71e864c880acd0
ivanhoe <corsair954@mail.ru>
2025-12-07 18:59:20 +0300

    test

---
+++ src/main/java/ru/job4j/newcoll/fortaskavl/TreeAVLMap.java
@@ -0,0 +1,47 @@
+package ru.job4j.newcoll.fortaskavl;
+
+import java.util.*;
+
+public class TreeAVLMap<T extends Comparable<T>, V> {
+
+    private Node root;
+
+    public boolean put(T key, V value) {
+        // TODO реализуйте метод
+        return false;
+    }
+
+    public boolean remove(T key) {
+        // TODO реализуйте метод
+        return false;
+    }
+
+    public V get(T key) {
+        // TODO реализуйте метод
+        return null;
+    }
+
+    public Set<T> keySet() {
+        // TODO реализуйте метод
+        return null;
+    }
+
+    public Collection<V> values() {
+        // TODO реализуйте метод
+        return null;
+    }
+
+    private class Node {
+        private int balanceFactor;
+        private T key;
+        private V value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        Node(T key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
